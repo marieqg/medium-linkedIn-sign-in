81d8cc2cb3e2d22a1a346ad36ece8873d4ea5dcc
marieqg <mariequittelier@gmail.com>
2020-10-11 16:14:35 +1300

    4

--- pages/api/oauth.js
+++ pages/api/oauth.js
@@ -1,14 +1,13 @@
 import { getURLWithQueryParams } from "../../helpers/auth";
-import Spinner from "../../assets/icons/Spinner";
 
 
-const Oauth = async (context) => {
+const Oauth = async (req, res) => {
   // Query to exchange our authorization code for an access token 
     const LINKEDIN_URL = getURLWithQueryParams(
         "https://www.linkedin.com/oauth/v2/accessToken",
         {
           grant_type: "authorization_code",
-          code: context.query.code,
+          code: req.query.code,
           redirect_uri: process.env.LINKEDIN_REDIRECT,
           client_id: process.env.LINKEDIN_CLIENT_ID,
           client_secret: process.env.LINKEDIN_CLIENT_SECRET
@@ -31,37 +30,10 @@ const Oauth = async (context) => {
     headers: { Connection: "Keep-Alive", Authorization: auth }
   });
   if (usr.ok) u = await usr.json();
-
-
-  return (
- <Spinner/>
-    );
+if (u.localizedFirstName) {
+  res.redirect(`/hello/${u.localizedFirstName}`)
+}
 };
 
 export default Oauth
 
-
-// Oauth.getInitialProps = async context => {
-
-  
-
-//   let em = {};
-//   let ema = await fetch(
-//     "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))",
-//     {
-//       method: "GET",
-//       headers: { Connection: "Keep-Alive", Authorization: auth }
-//     }
-//   );
-//   if (ema.ok) em = await ema.json();
-//   const name = u.localizedFirstName;
-//   const famillyName = u.localizedLastName;
-//   const email = em.elements[0]["handle~"]["emailAddress"];
-//   const password = access_token;
-//   const tokenExpiry = expires_in;
-//   const user = { name, famillyName, email, password, tokenExpiry };
-
-//   return { user, access_token, expires_in };
-// };
-
-// export default withApollo(Oauth);
