e79c49827e790d5cd67ddc13e640add959555f90
marieqg <mariequittelier@gmail.com>
2020-12-13 14:05:20 +1300

    code clean

--- pages/hello/[name].js
+++ pages/hello/[name].js
@@ -1,19 +1,15 @@
-import styles from '../../styles/Home.module.css'
-import { useRouter } from 'next/router'
+import styles from '../../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
- const Index = () => {
-    const router = useRouter()
-     const { name } = router.query
-     console.log(name)
-
-   return (
+const Index = () => {
+  const router = useRouter();
+  const { name } = router.query;
+  return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}> Hello {name}
-        </h1>
+        <h1 className={styles.title}> Hello {name}</h1>
       </main>
     </div>
-)
-   }
-export default Index
-
+  );
+};
+export default Index;
